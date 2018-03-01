04fb0719c7b5d5b5729a31b3a9163fa2a2e51229
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-03-01 18:17:00 -0300

    Colorview Component

--- challenge_one/src/components/ColorView.js
+++ challenge_one/src/components/ColorView.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
 
 class ColorView extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            color: '#F7F7F7'
+        }
+
+        this.onKeyUp = this.onKeyUp.bind(this)
+    }
+
+    onKeyUp(){
+        const color = this.refs.inputColor.value
+        color.includes("#") ? this.setState({ color }) : this.setState({ color:"rgb("+color+")" })
+    }
+
     render(){
+        const { color } = this.state
         return(
-            <div>
-                <h1 className="card-title pricing-card-title">XX <small className="text-muted">A</small></h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                    <li>Some Text</li>
-                </ul>
-                <button type="button" className="btn btn-lg btn-block btn-primary">Get Started</button>
+            <div className="card mb-4 box-shadow">
+            <div className="card-header" style={{"backgroundColor":color}}>
+              <h4 className="my-0 font-weight-normal">ColorView</h4>
             </div>
+            <div className="card-body">
+                <h1 className="card-title pricing-card-title">
+                <div style={{"backgroundColor":color}} className="colordivbig rounded">
+                    <div className="hexcolor">{color}</div>
+                </div>
+                </h1>
+                <div className="mt-3 mb-4">
+                    <input 
+                        ref="inputColor"
+                        onKeyUp={this.onKeyUp}
+                        maxLength="11"
+                        className="form-control" type="text" placeholder="Insert Color Value"/>
+                    <br/>
+                    <div className="alert alert-primary">
+                        How to Use:<br/>
+                        in Hexa: <strong>#FF00FF</strong><br/>
+                        in RGB: <strong>250,0,255</strong><br/>
+                        [only numbers with comma]
+                    </div>
+                </div>
+            </div>
+          </div>
         )
     }
 }
--- challenge_one/src/components/Main.js
+++ challenge_one/src/components/Main.js
@@ -9,19 +9,12 @@ const Main = () => {
         <div>
           <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h1 className="display-4">Challenge One</h1>
-            <p className="lead">Componente Countdown: dado uma data futura ou passada o componente mostra de maneira amigável quantos dias/horas/minutos/etc atrás ou tempo que falta para chegar a esta data. Pense nas possibilidades que este componentes pode ter, como texto quando a contagem chegar ao fim e idioma.</p>
+            <p className="lead">Componente ColorView: dado um código de cor em RGB (em decimal e em hexa) mostrar a cor em si (tratando a diferença entre decimal e hexa).</p>
           </div>
 
           <div className="container">
             <div className="card-deck mb-3 text-center">
-              <div className="card mb-4 box-shadow">
-                <div className="card-header">
-                  <h4 className="my-0 font-weight-normal">CountDown</h4>
-                </div>
-                <div className="card-body">
-                    <Countdown />
-                </div>
-              </div>
+              <Countdown />
               <div className="card mb-4 box-shadow">
                 <div className="card-header">
                   <h4 className="my-0 font-weight-normal">FriendDate</h4>
@@ -30,14 +23,7 @@ const Main = () => {
                     <FriendDate />
                 </div>
               </div>
-              <div className="card mb-4 box-shadow">
-                <div className="card-header">
-                  <h4 className="my-0 font-weight-normal">ColorView</h4>
-                </div>
-                <div className="card-body">
-                    <ColorView />
-                </div>
-              </div>
+              <ColorView />
             </div>
           </div>
         </div>

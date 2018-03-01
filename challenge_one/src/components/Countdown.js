41012f5f51a9f39a4c9688488999e5873abb8a5a
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-03-01 17:03:44 -0300

    Re factoring

--- challenge_one/src/components/Countdown.js
+++ challenge_one/src/components/Countdown.js
@@ -29,7 +29,11 @@ class Countdown extends Component{
 
     render(){
         return(
-            <div>
+            <div className="card mb-4 box-shadow">            
+                <div className="card-header">
+                  <h4 className="my-0 font-weight-normal">CountDown</h4>
+                </div>
+                <div className="card-body">
                 <h1 className="card-title pricing-card-title"><CountdownClock end={this.state.end}/></h1>
                 <div className="mt-3 mb-4">
                     <label>{this.state.end}</label>
@@ -42,6 +46,7 @@ class Countdown extends Component{
                 <button 
                     onClick={ () => this.changeEnd()}
                     type="button" className="btn btn-lg btn-block btn-outline-primary">Run!</button>
+                </div>
             </div>
         )
     }

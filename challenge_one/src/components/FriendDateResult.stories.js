4851423def6205c25bff701ddb85124d38199c10
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-03-18 01:43:05 -0300

    Update the storybook: friendlyDate

--- challenge_one/src/components/FriendDateResult.stories.js
+++ challenge_one/src/components/FriendDateResult.stories.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react'
 
 import FriendDateResult from './FriendDateResult'
 
-storiesOf('Friend Date', module)
-    .add('with default date', () => <FriendDateResult finalDate="2018-02-28"/>)
-    .add('with extensive date', () => <FriendDateResult type="extensive" finalDate="28 de Janeiro de 2019"/>)
+storiesOf('Friendly Date', module)
+    .add('with default date', () => <FriendDateResult finalDate="2018-02-28" type="default"/>)
+    .add('with extensive date', () => <FriendDateResult finalDate="2019-01-25" type="extensive"/>)
